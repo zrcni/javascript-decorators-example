79e09555684df2999a5ac8ff49566028a0141ba0
Samuli Rauatmaa <zrcni9@gmail.com>
2019-04-16 15:10:36 +0300

    added @log

--- src/index.js
+++ src/index.js
@@ -42,6 +42,24 @@ function logError(target, key, descriptor) {
   return descriptor;
 }
 
+function log(target, key, descriptor) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function(...args) {
+    try {
+      console.log("[LOGGER]: Telling a joke.");
+      const result = originalMethod.call(this, ...args);
+      console.log(`[LOGGER]: Told a joke: "${result}".`);
+    } catch (err) {
+      console.error(
+        `[LOGGER]: Could not tell a joke, because "${err.message}".`
+      );
+      throw err;
+    }
+  };
+  return descriptor;
+}
+
 class Joke {
   @readonly
   default = "default joke";
@@ -59,16 +77,24 @@ class Joke {
   error() {
     throw new Error("AN ERROR");
   }
+
+  @log
+  tell(joke) {
+    if (!joke) {
+      throw new Error("invalid joke");
+    }
+    return joke;
+  }
 }
 
 const joke = new Joke();
 
-const randomButton = document.querySelector("#random-joke");
+const randomButton = document.querySelector("#random-joke-button");
 randomButton.onclick = () => {
   joke.random();
 };
 
-const assignReadonlyButton = document.querySelector("#assign-readonly");
+const assignReadonlyButton = document.querySelector("#assign-readonly-button");
 assignReadonlyButton.onclick = () => {
   try {
     joke.default = "a new default joke";
@@ -77,9 +103,17 @@ assignReadonlyButton.onclick = () => {
   }
 };
 
-const errorJokeButton = document.querySelector("#error-joke");
+const errorJokeButton = document.querySelector("#error-joke-button");
 errorJokeButton.onclick = () => {
   try {
     joke.error();
+  } catch (err) {}
+};
+
+const tellJokeButton = document.querySelector("#tell-joke-button");
+tellJokeButton.onclick = () => {
+  try {
+    const tellJokeInput = document.querySelector("#tell-joke-input");
+    joke.tell(tellJokeInput.value);
   } catch (err) {}
 };
